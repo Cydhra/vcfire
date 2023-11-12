6a8a9ebd40fef654d92ced2583919a9c22588a77
Johannes Hengstler <ubezl@student.kit.edu>
2023-11-12 15:14:00 +0100

    implemented fast parsing by skipping unnecessary steps and don't allocate strings for each sample

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
+
 use flate2::read::MultiGzDecoder;
 
 pub struct VcfFile {
@@ -52,16 +53,16 @@ pub enum NonStandardInfoValue {
 
 #[derive(Debug)]
 pub struct VcfRecord {
-    chromosome: String,
-    position: u32,
+    chromosome: Option<String>,
+    position: Option<u32>,
     id: Option<Vec<String>>,
-    reference_bases: String,
+    reference_bases: Option<String>,
     alternate_bases: Option<Vec<Option<String>>>,
-    quality: f32,
-    filter_status: String,
-    info: Option<Vec<InfoEntry>>,
+    quality: Option<f32>,
+    filter_status: Option<String>,
+    info: Option<Vec<Option<InfoEntry>>>,
     end: Option<u32>,
-    sample_info: Vec<SampleInfo>,
+    sample_info: Option<SampleInfo>,
 }
 
 #[derive(Debug)]
@@ -69,7 +70,23 @@ pub struct SampleInfo {
     unparsed_info: String,
 }
 
-struct SampleIterator<'a> {
+pub mod parser_capabilities {
+    pub const CHROM: u16 = 0b0000000000000001;
+    pub const POS: u16 = 0b0000000000000010;
+    pub const ID: u16 = 0b0000000000000100;
+    pub const REF: u16 = 0b0000000000001000;
+    pub const ALT: u16 = 0b0000000000010000;
+    pub const QUAL: u16 = 0b0000000000100000;
+    pub const FILTER: u16 = 0b0000000001000000;
+    pub const INFO: u16 = 0b0000000010000000;
+    pub const END: u16 = 0b0000000100000000;
+    pub const FORMAT: u16 = 0b0000001000000000;
+    pub const SAMPLES: u16 = 0b0000010000000000;
+
+    pub const ALL: u16 = 0b0000011111111111;
+}
+
+struct SampleIterator<'a, const FLAGS: u16> {
     reader: Box<dyn BufRead>,
     header: &'a VcfHeader,
     buffer: String,
@@ -93,13 +110,14 @@ impl VcfFile {
     }
 
     // Open the VCF file and get a sequential lazy iterator over all samples
-    pub fn records(&self) -> io::Result<impl Iterator<Item=io::Result<VcfRecord>> + '_> {
-        let mut reader: Box<dyn BufRead> =
-            if self.compressed {
-                Box::new(BufReader::new(MultiGzDecoder::new(File::open(&self.path)?)))
-            } else {
-                Box::new(BufReader::new(File::open(&self.path)?))
-            };
+    pub fn records<const FLAGS: u16>(
+        &self,
+    ) -> io::Result<impl Iterator<Item = io::Result<VcfRecord>> + '_> {
+        let mut reader: Box<dyn BufRead> = if self.compressed {
+            Box::new(BufReader::new(MultiGzDecoder::new(File::open(&self.path)?)))
+        } else {
+            Box::new(BufReader::new(File::open(&self.path)?))
+        };
 
         let mut buf = String::new();
         loop {
@@ -112,7 +130,7 @@ impl VcfFile {
             }
         }
 
-        Ok(SampleIterator {
+        Ok(SampleIterator::<FLAGS> {
             reader,
             header: &self.header,
             buffer: String::with_capacity(1024),
@@ -125,7 +143,10 @@ impl VcfFile {
         let mut file_version = String::with_capacity(32);
 
         reader.read_line(&mut file_version)?;
-        assert!(file_version.starts_with("##fileformat="), "VCF file misses file format identifier");
+        assert!(
+            file_version.starts_with("##fileformat="),
+            "VCF file misses file format identifier"
+        );
         file_version.remove_matches("##fileformat=");
 
         // TODO parse rest of meta fields
@@ -170,45 +191,135 @@ impl VcfFile {
     }
 }
 
-impl<'a> SampleIterator<'a> {
+impl<'a, const FLAGS: u16> SampleIterator<'a, FLAGS> {
     pub(crate) fn parse_current_record(&self, header: &VcfHeader) -> VcfRecord {
-        let mut fields = self.buffer.split('\t');
+        let fields_without_samples =
+            8 + header.has_end_column as usize + header.sample_names.is_some() as usize;
+
+        let mut fields = self.buffer.splitn(fields_without_samples + 1, '\t');
 
         VcfRecord {
-            chromosome: fields.next().expect("VCF record empty").into(),
-            position: fields.next().expect("VCF record misses POS entry").parse().expect("VCF record has malformed POS entry"),
-            id: match fields.next().expect("VCF record misses ID entry") {
-                "." => None,
-                s => Some(s.split(';').map(|s| s.into()).collect())
-            },
-            reference_bases: fields.next().expect("VCF record misses REF entry").into(),
-            alternate_bases: match fields.next().expect("VCF record misses ALT entry") {
-                "." => None,
-                s => Some(s.split(',')
-                    .map(|s| match s {
-                        "." => None,
-                        s => Some(s.into())
-                    })
-                    .collect())
-            },
-            quality: fields.next().expect("VCF record misses QUAL entry").parse().expect("VCF record has malformed QUAL entry"),
-            filter_status: fields.next().expect("VCF record misses FILTER entry").into(), // todo parse this as an enum
-            info: fields.next().expect("VCF record misses INFO entry").split(';').map(|info| match info {
-                "." => None,
-                info => None, // todo parse
-            }).collect(),
-            end: if header.has_end_column { fields.next().expect(""); None } else { None }, // todo
-            sample_info: if header.sample_names.is_some() {
-                fields.next().expect("VCF record misses FORMAT entry");
-                fields.map(|s| SampleInfo { unparsed_info: s.into() }).collect()
+            chromosome: if FLAGS & parser_capabilities::CHROM != 0 {
+                Some(fields.next().expect("VCF record empty").into())
             } else {
-                Vec::new()
+                fields.next();
+                None
+            },
+            position: if FLAGS & parser_capabilities::POS != 0 {
+                Some(
+                    fields
+                        .next()
+                        .expect("VCF record misses POS entry")
+                        .parse()
+                        .expect("VCF record has malformed POS entry"),
+                )
+            } else {
+                fields.next();
+                None
+            },
+            id: if FLAGS & parser_capabilities::ID != 0 {
+                match fields.next().expect("VCF record misses ID entry") {
+                    "." => None,
+                    s => Some(s.split(';').map(|s| s.into()).collect()),
+                }
+            } else {
+                fields.next();
+                None
+            },
+            reference_bases: if FLAGS & parser_capabilities::REF != 0 {
+                Some(fields.next().expect("VCF record misses REF entry").into())
+            } else {
+                fields.next();
+                None
+            },
+            alternate_bases: if FLAGS & parser_capabilities::ALT != 0 {
+                match fields.next().expect("VCF record misses ALT entry") {
+                    "." => None,
+                    s => Some(
+                        s.split(',')
+                            .map(|s| match s {
+                                "." => None,
+                                s => Some(s.into()),
+                            })
+                            .collect(),
+                    ),
+                }
+            } else {
+                fields.next();
+                None
+            },
+            quality: if FLAGS & parser_capabilities::QUAL != 0 {
+                Some(
+                    fields
+                        .next()
+                        .expect("VCF record misses QUAL entry")
+                        .parse()
+                        .expect("VCF record has malformed QUAL entry"),
+                )
+            } else {
+                fields.next();
+                None
+            },
+            filter_status: if FLAGS & parser_capabilities::FILTER != 0 {
+                Some(
+                    fields
+                        .next()
+                        .expect("VCF record misses FILTER entry")
+                        .into(),
+                ) // todo parse this as an enum
+            } else {
+                fields.next();
+                None
+            },
+            info: if FLAGS & parser_capabilities::INFO != 0 {
+                Some(
+                    fields
+                        .next()
+                        .expect("VCF record misses INFO entry")
+                        .split(';')
+                        .map(|info| match info {
+                            "." => None,
+                            info => None, // todo parse
+                        })
+                        .collect(),
+                )
+            } else {
+                fields.next();
+                None
+            },
+            end: if FLAGS & parser_capabilities::END != 0 {
+                if header.has_end_column {
+                    fields.next().expect("");
+                    None // todo parse
+                } else {
+                    None
+                }
+            } else {
+                if header.has_end_column {
+                    fields.next();
+                }
+                None
+            },
+            sample_info: if FLAGS & parser_capabilities::SAMPLES != 0 {
+                if header.sample_names.is_some() {
+                    fields.next().expect("VCF record misses FORMAT entry");
+                    Some(SampleInfo {
+                        unparsed_info: fields
+                            .next()
+                            .expect("VCF record misses sample info entries")
+                            .into(),
+                    })
+                } else {
+                    None
+                }
+            } else {
+                None
             },
         }
     }
 }
 
-impl<'a> Iterator for SampleIterator<'a> {
+impl<'a, const FLAGS: u16> Iterator for SampleIterator<'a, FLAGS> {
     type Item = io::Result<VcfRecord>;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -222,27 +333,36 @@ impl<'a> Iterator for SampleIterator<'a> {
 
                 Some(Ok(self.parse_current_record(&self.header)))
             }
-            Err(e) => Some(Err(e))
+            Err(e) => Some(Err(e)),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use std::time::Instant;
+
+    use super::*;
 
     #[test]
     fn test() {
-        let mut vcf_file = VcfFile::parse("run/example.vcf.gz", true).expect("failed to open VCF file");
+        let vcf_file = VcfFile::parse("run/example.vcf.gz", true).expect("failed to open VCF file");
 
         let start = Instant::now();
         let mut cells = 0;
 
-        vcf_file.records().expect("failed to open VCF file")
+        vcf_file
+            .records::<{ parser_capabilities::SAMPLES }>()
+            .expect("failed to open VCF file")
             .take(40000)
             .for_each(|rec| {
-                cells += rec.expect("failed to parse VCF record").sample_info.len()
+                cells += rec
+                    .expect("failed to parse VCF record")
+                    .sample_info
+                    .expect("")
+                    .unparsed_info
+                    .split('\t')
+                    .count()
             });
         println!("read {} cells in {:?}", cells, start.elapsed());
     }
